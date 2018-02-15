89f319d657bf2a3adbb7ca0cef688c1a261b6c28
Dean Nabee windows8 <suprnatl@outlook.om>
2018-02-15 18:12:44 +0400

    added files

---
+++ config.rb
@@ -0,0 +1,18 @@
+css_dir = "assets/css"
+sass_dir = "assets/sass"
+images_dir = "assets/img"
+javascripts_dir = "assets/js"
+fonts_dir = "assets/fonts"
+
+output_style = :nested
+
+environment = :development
+
+sass_options = { :sourcemap => false }
+
+relative_assets = true
+
+sourcemap = false
+
+asset_cache_buster = :none
+cache = false
