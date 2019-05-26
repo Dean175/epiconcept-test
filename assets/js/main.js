107df07b9a3e13d2bb4c41d70ee078c362ff45e5
Dean175 <daredvl175@outlook.com>
2019-05-26 21:56:25 +0400

    implement login form
    implement login form and email validation through regex

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
-// Initialize the slider
 $(document).ready(function(){
-
+	// Initialize the slider
 	$('.slides').bxSlider({
 		mode: 'fade',
 	    slideWidth: 1035,
@@ -9,4 +8,42 @@ $(document).ready(function(){
 	    pager: false
 	});
 
+});
+
+const $connectBtn = document.getElementById("connect-button");
+const $loginFrmBox = document.querySelector(".login-form-box");
+const $loginFrm = document.querySelector(".login-form");
+
+const $email = document.querySelector("#email");
+console.log($email.value);
+
+
+
+$connectBtn.addEventListener("click", function(e){
+	e.preventDefault();
+	$loginFrmBox.classList.add("show");
+});
+
+// Detect all clicks on the document
+document.addEventListener("click", function(event) {
+	// If user clicks inside the element, do nothing
+	if (event.target.closest("#connect-button") || event.target.closest(".login-form-box")) return;
+
+	// If user clicks outside the element, hide it!
+	$loginFrmBox.classList.remove("show");
+});
+
+$loginFrm.addEventListener("submit", function(e){
+	
+	const emailRegex = /\w+@\w+\.\w{2,}/;
+
+	let email = $email.value;
+
+	if(emailRegex.test(email)) {
+		console.log("valid mail");
+		
+	} else {
+		e.preventDefault();
+	}
+	
 });
